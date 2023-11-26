48ff4acd76f7a4d0f575ddafd8fcbfcbee1da58f
JAbelP <59974707+JAbelP@users.noreply.github.com>
2023-11-26 17:33:43 -0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -10,37 +10,5 @@ export default function Home() {
 <Head>
         <meta http-equiv="refresh" content="0;url=https://inarishrinecafe.org/"/>
       </Head>
-      <div className=''>
-        <Image
-          className='mx-auto mb-8 pt-8'
-          src = '/Logo/Logo.svg'
-          width={46}
-          height={46}
-          
-        />    
-      </div>
-      <div className="flex justify-center">
-        <SliderHP />
-      </div>
-      <div className='absolute top-[39rem]'>
-        <Image
-        src='/Chibi/TinyChibiWithTwoFoxes.png'
-        width={100}
-        height={100}
-        />
-      </div>
-    <div className='mb-16 ml-8 mt-4'>
-      <EyeCatch title={"what is a manga cafe?"} description={"At a Manga Cafe, You can grab a drink and read the latest manga or popular clasicss! "}/>
-
-    </div>
-   
-    <div className='flex justify-end mb-16 mr-8'>
-      <EyeCatch title={"What does Inari Mean?"} description={"Inari is The God of harvests, fertility, rice, agriculture, foxes, industry, and worldly success."}/>
-    </div>
-     
-      <div className='mx-16'>
-        <SocialMediaBox/>
-      </div>
-    </div>
-  )
+      )
 }
