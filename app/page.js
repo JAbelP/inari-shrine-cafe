3df47b8b0a0a08b86063bc87a1a4f2673bdcbfe0
JAbelP <59974707+JAbelP@users.noreply.github.com>
2023-11-26 18:06:25 -0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import { useEffect } from 'react';
 const YourComponent = () => {
   useEffect(() => {
     // Replace 'https://example.com' with the URL you want to redirect to
-    window.location.replace('https://example.com');
+    window.location.replace('https://inarishrinecafe.org');
   }, []);
 
   return (
