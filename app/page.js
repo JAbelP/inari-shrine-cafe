ee4bf4749e12a65fc9f612d212e3a5d42deea37f
JAbelP <59974707+JAbelP@users.noreply.github.com>
2023-11-26 17:36:41 -0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,14 +1,12 @@
-import Image from 'next/image'
-import EyeCatch from './components/eyeCatch'
-import SocialMediaBox from './components/socialMediaComponents/socialMediaBox'
-import SliderHP from './components/sliderHomePage/SliderHP'
+
 import Head from 'next/head'
  
 export default function Home() {
   return (
-    <div className='h-max  text-black w-fit bg-[#FF8584]'>
+    <div>
 <Head>
         <meta http-equiv="refresh" content="0;url=https://inarishrinecafe.org/"/>
       </Head>
+</div>
       )
 }
