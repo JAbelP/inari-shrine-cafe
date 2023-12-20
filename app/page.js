df187945529c45ba6378a8514e272c2b93dabfcc
JAbelP <59974707+JAbelP@users.noreply.github.com>
2023-12-20 09:26:24 -0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import { useEffect } from 'react';
 const YourComponent = () => {
   useEffect(() => {
     // Replace 'https://example.com' with the URL you want to redirect to
-    window.location.replace('https://inarishrinecafe.org');
+    window.location.replace('https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley');
   }, []);
 
   return (
