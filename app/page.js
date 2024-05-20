605076d48937cffcf943bb6ad7b80e3d8e6eceee
JAbelP <abelpinales97@gmail.com>
2024-05-20 03:09:15 -0400

    Back to normal >:)

--- app/page.js
+++ app/page.js
@@ -1,13 +1,15 @@
-"use client";
-import { useEffect } from "react";
+"use client"
+import { useEffect } from 'react';
 
 const YourComponent = () => {
   useEffect(() => {
     // Replace 'https://example.com' with the URL you want to redirect to
-    // window.location.replace('https://inarishrinecafe.org/');
+    window.location.replace('https://inarishrinecafe.org/');
   }, []);
 
-  return <>helo world</>;
+  return (
+   <></>
+  );
 };
 
 export default YourComponent;
